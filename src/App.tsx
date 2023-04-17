37c24620df4f7b4c9abf57ca583f28df95964875
JayDouglass <jay.douglass@gmail.com>
2023-04-17 01:53:21 -0500

    add docs

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
 export default App;
